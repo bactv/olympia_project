3ef0616b140328562f4d28167b4e7ee91baedc2c
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 00:36:02 +0700

    Add DefaultController cms

---
+++ backend/controllers/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 12:31 SA
+ */
+namespace backend\controllers;
+use backend\components\BackendController;
+use Yii;
+
+class DefaultController extends BackendController
+{
+    public function index()
+    {
+
+    }
+}
