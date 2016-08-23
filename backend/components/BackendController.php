c84d14248995f617401bb47b495d11ff8b99506c
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 00:37:52 +0700

    Add BackendController to permit user

---
+++ backend/components/BackendController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 12:33 SA
+ */
+namespace backend\components;
+use Yii;
+use yii\web\Controller;
+
+class BackendController extends Controller
+{
+    
+}
