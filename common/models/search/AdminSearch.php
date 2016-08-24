073076e1d6f63b66005463aaa63c279d51726ff8
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 23:09:09 +0700

    Add AdminSearch

---
+++ common/models/search/AdminSearch.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Admin;
+
+/**
+ * AdminSearch represents the model behind the search form about `backend\models\Admin`.
+ */
+class AdminSearch extends Admin
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status', 'deleted', 'created_by', 'updated_by'], 'integer'],
+            [['username', 'password', 'email', 'fullname', 'created_time', 'updated_time', 'last_login_time'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Admin::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'status' => $this->status,
+            'deleted' => $this->deleted,
+            'created_time' => $this->created_time,
+            'updated_time' => $this->updated_time,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+            'last_login_time' => $this->last_login_time,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'password', $this->password])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'fullname', $this->fullname]);
+
+        return $dataProvider;
+    }
+}
