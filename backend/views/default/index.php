d38df30f33475183bc068b1ddb99d18b8b37614b
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 12:22:27 +0700

    Add default view backend

---
+++ backend/views/default/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 10:39 SA
+ */
---
+++ backend/views/default/login.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 7:43 SA
+ */
+
+use yii\helpers\Html;
+use yii\captcha\Captcha;
+use kartik\form\ActiveForm;
+
+$this->title = Yii::$app->name . ' - ' . Yii::t('cms', 'Login');
+?>
+<div id="form-login">
+    <h3 class="text-center form-title"><?php echo Yii::t('cms', 'Login CMS'); ?></h3>
+    <?php $form = ActiveForm::begin([
+        'options' => [
+            'class' => 'm-t',
+            'role' => 'form'
+        ]
+    ]); ?>
+    <?php if(Yii::$app->session->hasFlash('error')): ?>
+        <div class="alert alert-danger" role="alert">
+            <?= Yii::$app->session->getFlash('error') ?>
+        </div>
+    <?php endif; ?>
+    <div class="form-group">
+        <?php echo $form->field($model, 'username', [
+            'addon' => ['prepend' => ['content' => "<i class=\"fa fa-user\" aria-hidden=\"true\"></i>"]]
+        ])->textInput(['class' => 'form-control', 'placeholder' => Yii::t('cms', 'Username'), 'required' => ''])->label(false); ?>
+    </div>
+    <div class="form-group">
+        <?php echo $form->field($model, 'password', [
+            'addon' => ['prepend' => ['content' => "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i>"]]
+        ])->passwordInput(['class' => 'form-control', 'placeholder' => Yii::t('cms', 'Password'), 'required' => ''])->label(false); ?>
+    </div>
+    <div class="form-group">
+        <?php echo $form->field($model, 'verifyCode')->widget(Captcha::className(), ['captchaAction' => 'default/captcha', 'imageOptions' => ['width' => '99', 'height' => '32'], 'template' => '<div class="row"><div class="col-lg-6">{input}</div><div class="col-lg-4">{image}</div><a href="javascript:;" id="capcha-refresh-button"><i class="fa fa-refresh f-s-18"></i></a></div>', 'options' => ['placeholder' => Yii::t('cms', 'verifyCode'), 'class' => 'form-control']])->label(false); ?>
+    </div>
+    <div class="form-group">
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+        <?php echo Html::submitButton(Yii::t('cms', 'Login') . " <i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i>", ['class' => 'btn btn-primary block full-width m-b']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+    <br/>
+    <?php
+//    yii\authclient\widgets\AuthChoice::widget([
+//        'baseAuthUrl' => ['google/auth'],
+//        'id' => 'google_auth'
+//    ])
+    ?>
+    <p class="m-t text-center"><small>&copy; Copyright by bactv <?php echo date("Y") ?></small></p>
+</div>
