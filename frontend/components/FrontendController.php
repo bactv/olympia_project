5bae834ff19ff829c2bb76c0c0313ed658d4615f
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 23:10:49 +0700

    Add FrontendController

---
+++ frontend/components/FrontendController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 8:49 CH
+ */
+namespace frontend\components;
+
+use Yii;
+use yii\web\Controller;
+
+class FrontendController extends Controller
+{
+    
+}
