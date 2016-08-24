06867adeeaa3040eb94265462646ba1eaefca959
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 12:20:41 +0700

    Add CategoryMenu backend models

---
+++ backend/models/CategoryMenu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 12:05 CH
+ */
+namespace backend\models;
+
+use common\models\CategoryMenuBase;
+use Yii;
+
+class CategoryMenu extends CategoryMenuBase
+{
+
+}
