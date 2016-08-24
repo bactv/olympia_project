238551b9df2eab5555489aa3af18e4e3ac9b00ad
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 12:18:50 +0700

    Update DefaultController backend

--- backend/controllers/DefaultController.php
+++ backend/controllers/DefaultController.php
@@ -7,12 +7,61 @@
  */
 namespace backend\controllers;
 use backend\components\BackendController;
+use backend\models\LoginForm;
 use Yii;
 
 class DefaultController extends BackendController
 {
-    public function index()
+    /**
+     * @return array
+     */
+    public function actions()
     {
+        return [
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+            ],
+        ];
+    }
 
+    /**
+     * @return string|\yii\web\Response
+     */
+    public function actionIndex()
+    {
+        if (Yii::$app->user->isGuest)
+            return $this->redirect(['default/login']);
+        else
+            return $this->render('index');
+    }
+
+    /**
+     * @return string|\yii\web\Response
+     */
+    public function actionLogin()
+    {
+        $this->layout = 'login';
+
+        if (!Yii::$app->user->isGuest)
+            return $this->goHome();
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            Yii::$app->cache->delete('cacheListMenuFunction');
+            return $this->goBack();
+        } else {
+            return $this->render('login', [
+                'model' => $model
+            ]);
+        }
+    }
+
+    /**
+     * @return \yii\web\Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+        return $this->goHome();
     }
 }
