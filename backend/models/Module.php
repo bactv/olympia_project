65e519d161e7e694807f65e3fd7e58f3a8111202
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 12:21:27 +0700

    Add Module backend models

---
+++ backend/models/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 24/08/2016
+ * Time: 11:59 SA
+ */
+namespace backend\models;
+
+use common\models\ModuleBase;
+use Yii;
+
+class Module extends ModuleBase
+{
+    
+}
