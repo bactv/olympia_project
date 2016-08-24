dfa9ad4232a0a71666060a28ee0e0b98a8c02dcb
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 12:25:53 +0700

    Update main config common

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,31 @@
 <?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@cms', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@web', dirname(dirname(__DIR__)) . '/frontend');
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages'
+                ],
+                'cms' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages'
+                ],
+                'web' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages'
+                ],
+            ]
+        ]
     ],
+    'language' => 'vi',
+    'sourceLanguage' => 'en',
 ];
