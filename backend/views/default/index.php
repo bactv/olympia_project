09feedb7ba9e7202839ed3c6f13b3676fd501f5a
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-24 23:05:11 +0700

    Update default view backend

--- backend/views/default/index.php
+++ backend/views/default/index.php
@@ -5,3 +5,17 @@
  * Date: 24/08/2016
  * Time: 10:39 SA
  */
+use yii\helpers\Html;
+
+$this->title = Yii::t('cms', 'DashBoard');
+
+?>
+
+<p id="hello-dashboard">Olympia CMS</p>
+<style>
+    p#hello-dashboard {
+        text-align: center;
+        font-size: 100px;
+        color: darkgray;
+    }
+</style>
