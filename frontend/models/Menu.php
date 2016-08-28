052c89874dad0c68c248df2a49e0616e2054580f
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-28 17:32:16 +0700

    Update frontend models

---
+++ frontend/models/Menu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+
+class Menu extends \common\models\MenuBase{
+
+
+}
---
+++ frontend/models/Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+
+class Module extends \common\models\ModuleBase{
+
+
+}
