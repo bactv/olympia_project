9106a6341d04f491fdbc0982cd97ea7377ff385a
Bac Truong Van <bactruongvan174@gmail.com>
2016-08-28 17:33:26 +0700

    Update common components

---
+++ common/components/MenuWidget.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 27/08/2016
+ * Time: 11:28 SA
+ */
+namespace common\components;
+
+use Yii;
+use yii\widgets\Menu;
+
+class MenuWidget extends Menu
+{
+    
+}
---
+++ common/components/Utility.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bac_b
+ * Date: 27/08/2016
+ * Time: 8:59 SA
+ */
+namespace common\components;
+
+class Utility
+{
+
+}
