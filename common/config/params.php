dd1b52dde0374f57d397f50a6ecb30f3384e5c1a
bactv <bactv@vega.com.vn>
2016-09-01 15:16:40 +0700

    Update common config

--- common/config/params.php
+++ common/config/params.php
@@ -6,7 +6,36 @@ return [
     'apps'=> ['backend', 'frontend'],
 
     'type_question' => [
-        'multiple-choice' => 2,
         'reply' => 1,
+        'multiple-choice' => 2,
+    ],
+
+    'type_game' => [
+        'game_week' => 1,
+        'game_month' => 2,
+        'game_quarters' => 3,
+        'game_year' => 4,
+    ],
+
+    'part_game' => [
+        'start' => 1,
+        'obstacle_race' => 2,
+        'accelerate' => 3,
+        'end' => 4
+    ],
+
+    'question_level' => [
+        'easy' => [
+            'id' => 1,
+            'probability' => 50
+        ],
+        'medium' => [
+            'id' => 1,
+            'probability' => 30
+        ],
+        'hard' => [
+            'id' => 3,
+            'probability' => 20
+        ]
     ]
 ];
